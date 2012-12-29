3ab850693a162b413f57afa51e003ee7de28b5e2
Rowan Merewood <rowan@merewood.org>
2012-12-29 15:33:51 +0000

    Convert integers up to one hundred

--- src/Kata/NumberNames/IntegerToWords.php
+++ src/Kata/NumberNames/IntegerToWords.php
@@ -27,8 +27,31 @@ class IntegerToWords
         19 => 'nineteen',
     );
 
+    private $multiplesOfTen = array(
+        2 => 'twenty',
+        3 => 'thirty',
+        4 => 'forty',
+        5 => 'fifty',
+        6 => 'sixty',
+        7 => 'seventy',
+        8 => 'eighty',
+        9 => 'ninety',
+    );
+
     public function convert(Integer $integer)
     {
-        return $this->uniqueNames[$integer->getValue()];
+        if ($integer->getValue() < 20) {
+            return $this->uniqueNames[$integer->getValue()];
+        }
+
+        $quotient = floor($integer->getValue() / 10);
+        $word = $this->multiplesOfTen[$quotient];
+        $remainder = $integer->getValue() % 10;
+
+        if ($remainder > 0) {
+            $word .= '-' . $this->uniqueNames[$remainder];
+        }
+
+        return $word;
     }
 }
